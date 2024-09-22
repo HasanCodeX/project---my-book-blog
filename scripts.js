3a38764df7a6c76fc4f029f43fa7eb9d2d7b7499
Mehedi Hasan <dev.hasan10@gmail.com>
2024-09-22 12:50:15 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,1 @@
-// Function to show books based on the year clicked with animation
-function showBooks(year) {
-    // Get all sections
-    const sections = document.querySelectorAll('.books-section');
-    
-    // Hide all sections
-    sections.forEach(section => {
-        section.style.display = 'none'; // Hide all sections
-        section.classList.remove('active'); // Remove active class
-    });
 
-    // Show the selected section
-    const selectedSection = document.getElementById(`${year}-books`);
-    selectedSection.style.display = 'block'; // Show selected section
-    setTimeout(() => {
-        selectedSection.classList.add('active'); // Add active class for animation
-    }, 100);
-}
